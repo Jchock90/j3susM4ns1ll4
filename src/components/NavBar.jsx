ff27363da3a5b68cee376667169f78caadce8b1e
Jesús Mansilla <99669921+Jchock90@users.noreply.github.com>
2024-08-22 15:44:56 -0300

    actualizaciones varias

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -27,9 +27,9 @@ const Navbar = () => {
           <Typewriter words={words} />
         </Link>
         <div className="hidden md:flex space-x-6 items-center">
-          <Link to="/gallery" className="text-lightGray hover:text-violet transition">{t('Galería')}</Link>
           <Link to="/" className="text-lightGray hover:text-violet transition">{t('Audio')}</Link>
           <Link to="/soundtrack" className="text-lightGray hover:text-violet transition">{t('Soundtrack')}</Link>
+          <Link to="/gallery" className="text-lightGray hover:text-violet transition">{t('Galería')}</Link>
           <Link to="/contact" className="text-lightGray hover:text-violet transition">{t('Contacto')}</Link>
           
           {/* Botones de selección de idioma */}
@@ -51,9 +51,9 @@ const Navbar = () => {
 
       {isOpen && (
         <div className="md:hidden flex justify-center items-center space-x-2 mt-2">
-          <Link to="/gallery" className="text-lightGray hover:text-violet transition text-sm">{t('Galería')}</Link>
           <Link to="/" className="text-lightGray hover:text-violet transition text-sm">{t('Audio')}</Link>
           <Link to="/soundtrack" className="text-lightGray hover:text-violet transition text-sm">{t('Soundtrack')}</Link>
+          <Link to="/gallery" className="text-lightGray hover:text-violet transition text-sm">{t('Galería')}</Link>
           <Link to="/contact" className="text-lightGray hover:text-violet transition text-sm">{t('Contacto')}</Link>
           
           <div className="flex space-x-2">
@@ -71,3 +71,4 @@ const Navbar = () => {
 };
 
 export default Navbar;
+
